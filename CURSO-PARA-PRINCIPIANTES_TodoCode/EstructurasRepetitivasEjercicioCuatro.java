72172b6dcee4ebc8fd3fdf31e7a6b83ac7b0532e
Juan González <juanglezg23@gmail.com>
2025-01-22 17:40:43 -0400

    Delete CURSO-PARA-PRINCIPIANTES_TodoCode directory

--- CURSO-PARA-PRINCIPIANTES_TodoCode/EstructurasRepetitivasEjercicioCuatro.java
+++
@@ -1,9 +0,0 @@
-public class EstructurasRepetitivasEjercicioCuatro {
-   public static void main(String[] args) {
-      int contador = 10;
-      while (contador > 0) {
-         System.out.println(contador);
-         contador--;
-      }
-   }
-}
--- CURSO-PARA-PRINCIPIANTES_TodoCode/EstructurasRepetitivasEjercicioUno.java
+++
@@ -1,9 +0,0 @@
-public class EstructurasRepetitivasEjercicioUno {
-   public static void main(String[] args) {
-      int contador = 0;
-      while (contador < 35) {
-         System.out.println(contador + 1);
-         contador++;
-      }
-   }
-}
